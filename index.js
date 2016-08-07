cd04c61891692e1e07c74e5ab7cec9fcb22630fb
Paul G <Pauldg7@gmail.com>
2016-08-07 17:39:01 -0500

    Removed other node api's example code.

--- index.js
+++ index.js
@@ -52,53 +52,6 @@ login.login(users, pass).then(token => {
     }, 10 * 1000);
 });
 
-/*var location = {
-    type: 'name',
-    name: process.env.PGO_LOCATION || 'Times Square'
-};
-
-a.init(users, pass, location, provider, function(err) {
-    if (err) throw err;
-
-    console.log('1[i] Current location: ' + a.playerInfo.locationName);
-    console.log('1[i] lat/long/alt: : ' + a.playerInfo.latitude + ' ' + a.playerInfo.longitude + ' ' + a.playerInfo.altitude);
-
-    a.GetProfile(function(err, profile) {
-        if (err) throw err;
-
-        console.log('1[i] Username: ' + profile.username);
-        console.log('1[i] Poke Storage: ' + profile.poke_storage);
-        console.log('1[i] Item Storage: ' + profile.item_storage);
-
-        var poke = 0;
-        if (profile.currency[0].amount) {
-            poke = profile.currency[0].amount;
-        }
-
-        console.log('1[i] Pokecoin: ' + poke);
-        console.log('1[i] Stardust: ' + profile.currency[1].amount);
-
-        setInterval(function(){
-            a.Heartbeat(function(err,hb) {
-                if(err) {
-                    console.log(err);
-                }
-
-                for (var i = hb.cells.length - 1; i >= 0; i--) {
-                    if(hb.cells[i].NearbyPokemon[0]) {
-                        //console.log(a.pokemonlist[0])
-                        var pokemon = a.pokemonlist[parseInt(hb.cells[i].NearbyPokemon[0].PokedexNumber)-1];
-                        console.log('1[+] There is a ' + pokemon.name + ' at ' + hb.cells[i].NearbyPokemon[0].DistanceMeters.toString() + ' meters');
-                    }
-                }
-
-            });
-        }, 5000);
-
-    });
-});*/
-
-
 app.listen(port);
 console.log("Server started at 127.0.0.1:" + port);
 
