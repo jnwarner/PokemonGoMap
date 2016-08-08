7c7c7a7693f2162a6082b11e20aada1846243125
Paul G <Pauldg7@gmail.com>
2016-08-08 02:25:19 -0500

    Worked on Basic Lat and Long offset functions

--- res/js/util.js
+++ res/js/util.js
@@ -30,7 +30,7 @@ module.exports = {
     }).then(() => {
     	console.log('Authenticated, waiting for first map refresh (10s)');
         setInterval(() => {
-            var cellIDs = pogobuf.Utils.getCellIDs(lat, long);
+            var cellIDs = pogobuf.Utils.getCellIDs(lat, long, 5);
             return bluebird.resolve(client.getMapObjects(cellIDs, Array(cellIDs.length).fill(0))).then(mapObjects => {
                 return mapObjects.map_cells;
             }).each(cell => {
@@ -38,11 +38,31 @@ module.exports = {
                 console.log('Has ' + cell.catchable_pokemons.length + ' catchable Pokemon');
                 return bluebird.resolve(cell.catchable_pokemons).each(catchablePokemon => {
                     console.log(' - A ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId,
-                        catchablePokemon.pokemon_id) + ' is asking you to catch it.');
+                        catchablePokemon.pokemon_id) + 'id '+catchablePokemon.pokemon_id+' IV of '+/*pogobuf.Utils.getIVsFromPokemon(catchablePokemon)+*/' is asking you to catch it.');
                 });
             });
         }, 10 * 1000);
     }).catch(console.error);
-  }
+  },
+
+  /*offset: function(origin){ //Current Position
+      var length = 56.568542494923802; //radius length of square (Take Proximity circle == 80meter diameter)
+      var l2 = length + (length/2);
+      var travel = [this.distance(origin, l2, 0), this.distance(origin, -l2, 0), this.distance(origin, l2, length/2), this.distance(origin, l2, -length/2), this.distance(origin, -l2, length/2), this.distance(origin, -l2, -length/2)];
+      return travel;
+  },
+
+  distance: function(origin, x,y){
+    var R = 6378137;
+    var lat = origin[0];
+    var long = origin[1];
+    var newLat = y/R;
+    var newLong = x/(R*Math.cos(Math.PI*lat/180));
+
+    //OffsetPosition, decimal degrees
+    var latO = lat + newLat * 180/Math.PI;
+    var longO = long + newLong * 180/Math.PI;
+    return [latO, longO];
+  }*/
 
 }
