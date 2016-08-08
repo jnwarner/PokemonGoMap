6796518bbb8784fb70c46c13d1dff376853c7f87
Paul G <Pauldg7@gmail.com>
2016-08-07 19:26:21 -0500

    Created Search function.
    Removed login code from index.js and moved it to util.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
+
+var search = require('./res/js/util.js');
+
 const pogobuf = require('pogobuf'),
 	  POGOProtos = require('node-pogo-protos'),
 	  bluebird = require('bluebird'),
@@ -16,42 +19,12 @@ var apiKey = config.api_key;
 var loginMethod = config.auth;
 var users = config.accounts;
 var pass = config.pass;
-var lat = parseFloat(config.lat);
-var long = parseFloat(config.long);
+var lat = config.lat;	//String will be converted to float in search (Same for long)
+var long = config.long;
 
-if (loginMethod.toLowerCase() == 'ptc') {
-	var login = new pogobuf.PTCLogin();
-} else {
-	var login = new pogobuf.GoogleLogin();
-}
+var location = [lat, long];
 
-var client = new pogobuf.Client();
-
-login.login(users, pass).then(token => {
-	// Initialize client
-	client.setAuthInfo(loginMethod, token);
-	client.setPosition(lat, long);
-	client.on('request', console.dir);
-  client.on('response', console.dir);
-	console.log('Logging into '+users+' with loginMethod '+loginMethod+' at Lat '+lat+' Long '+long);
-	return client.init();
-}).then(() => {
-	console.log('Authenticated, waiting for first map refresh (10s)');
-    setInterval(() => {
-        var cellIDs = pogobuf.Utils.getCellIDs(lat, long);
-        return bluebird.resolve(client.getMapObjects(cellIDs, Array(cellIDs.length).fill(0))).then(mapObjects => {
-            return mapObjects.map_cells;
-        }).each(cell => {
-            console.log('Cell ' + cell.s2_cell_id.toString());
-            console.log('Has ' + cell.catchable_pokemons.length + ' catchable Pokemon');
-            return bluebird.resolve(cell.catchable_pokemons).each(catchablePokemon => {
-                console.log(' - A ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId,
-                    catchablePokemon.pokemon_id) + ' is asking you to catch it.');
-            });
-        });
-    }, 10 * 1000);
-})
-.catch(console.error);;
+search.search(loginMethod, users, pass, location);
 
 app.listen(port);
 console.log("Server started at 127.0.0.1:" + port);
@@ -59,13 +32,12 @@ console.log("Server started at 127.0.0.1:" + port);
 function handler (req, res) {
     fs.readFile(__dirname + '/map.html',
     function (err, data) {
-	if (err) {
-    	res.writeHead(500);
-    	return res.end('Error loading... Curl up and cri everytime');
-    }
-
-    res.writeHead(200);
-    res.end(data);
+			if (err) {
+    		res.writeHead(500);
+    		return res.end('Error loading... Curl up and cri everytime');
+    	}
+    	res.writeHead(200);
+    	res.end(data);
   });
 }
 
---
+++ res/js/util.js
@@ -0,0 +1,48 @@
+const pogobuf = require('pogobuf'),
+	  POGOProtos = require('node-pogo-protos'),
+	  bluebird = require('bluebird'),
+	  Long = require('long');
+module.exports = {
+  search: function (auth, user, pass, location) {
+    if (auth.toLowerCase() == 'ptc') {
+    	var login = new pogobuf.PTCLogin();
+    } else {
+    	var login = new pogobuf.GoogleLogin();
+    }
+
+    var client = new pogobuf.Client();
+
+    if(location.length < 2){
+      //TODO
+    }else{
+      var lat = parseFloat(location[0]);
+      var long = parseFloat(location[1]);
+    }
+
+    login.login(user, pass).then(token => {
+      // Initialize client
+    	client.setAuthInfo(auth, token);
+    	client.setPosition(lat, long);
+    	client.on('request', console.dir);
+      client.on('response', console.dir);
+    	console.log('Logging into '+user+' with loginMethod '+auth+' at Lat '+lat+' Long '+long);
+    	return client.init();
+    }).then(() => {
+    	console.log('Authenticated, waiting for first map refresh (10s)');
+        setInterval(() => {
+            var cellIDs = pogobuf.Utils.getCellIDs(lat, long);
+            return bluebird.resolve(client.getMapObjects(cellIDs, Array(cellIDs.length).fill(0))).then(mapObjects => {
+                return mapObjects.map_cells;
+            }).each(cell => {
+                console.log('Cell ' + cell.s2_cell_id.toString());
+                console.log('Has ' + cell.catchable_pokemons.length + ' catchable Pokemon');
+                return bluebird.resolve(cell.catchable_pokemons).each(catchablePokemon => {
+                    console.log(' - A ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId,
+                        catchablePokemon.pokemon_id) + ' is asking you to catch it.');
+                });
+            });
+        }, 10 * 1000);
+    }).catch(console.error);
+  }
+
+}
